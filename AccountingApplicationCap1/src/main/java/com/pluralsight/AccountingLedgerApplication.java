3b4ff5e7cc95700185917ba12f5a16f02954fa6a
Yordiana0912 <Yordiana0912@gmail.com>
2024-05-01 16:13:26 -0400

    completed some work still missing some work

--- AccountingApplicationCap1/src/main/java/com/pluralsight/AccountingLedgerApplication.java
+++ AccountingApplicationCap1/src/main/java/com/pluralsight/AccountingLedgerApplication.java
@@ -1,67 +1,102 @@
 package com.pluralsight;
 
-import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class AccountingLedgerApplication {
-    public static <Transaction> void main(String[] args) {
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        //scanner added to start taking user input
         System.out.println("Welcome to your financing application ");
-        // have the user enter debit information
+        System.out.println("Please select what you would like to do ?");
+        System.out.println("1.Deposit , 2. Make a payment , 3.View Ledger");
+// providing client options on what they would like to do
+        int userresponces = Integer.parseInt(scanner.nextLine());
+        // we would recieved user input
 
-        System.out.println(" Please enter your deposit information");
-        System.out.println("Date(YYYY-MM-DD: ");
-        String date = scanner.nextLine();
+        switch (userresponces) {
+            case 1:
+                System.out.println(" Please enter your deposit information");
+                System.out.println("Date(YYYY-MM-DD: ");
+                String date = scanner.nextLine();
 
-        System.out.println("Name of deposit");
-        String name = scanner.nextLine();
+                System.out.println("Name of deposit");
+                String name = scanner.nextLine();
 
-        System.out.println("How much is the deposit:  ");
-        Double amount = Double.valueOf(scanner.nextLine());
+                System.out.println("How much is the deposit:  ");
+                Double amount = Double.valueOf(scanner.nextLine());
 
-        System.out.println("Please enter debit information");
-        String debitInfo = scanner.nextLine();
+                System.out.println("Please enter debit information");
+                String debitInfo = scanner.nextLine();
+                break;
 
-        class TransactionFileCreator {
-            public static void main(String[] args) {
-                List<Transaction> transactions = new ArrayList<>();
+            case 2:
+                System.out.println(" Please enter your paymentinformation");
+                System.out.println("Date(YYYY-MM-DD: ");
+                String datepayment = scanner.nextLine();
 
-                // Add sample transactions
-                transactions.add(new Transaction("2024-05-01", "Rent", 2600.00, "Deposit"));
-                transactions.add(new Transaction("2024-05-12", "Apple Music ", 20.00, "Withdrawal"));
-                transactions.add(new Transaction("2024-05-29", "Chic fil A", 50.00, "Withdrawal"));
+                System.out.println("Name of payment ");
+                String namepayment = scanner.nextLine();
 
+                System.out.println("How much is the payment :  ");
+                Double amountpayment = Double.valueOf(scanner.nextLine());
 
-                try {
-            FileWriter writer = new FileWriter("transactions.csv");
-            writer.write("Date, Name, Amount , DebitInfo \n");
+                System.out.println("Please enter debit payment ");
+                String debitInfopayment = scanner.nextLine();
+
+                break;
+
+            case 3:
+                System.out.println(" Please enter your Ledger information ");
+                System.out.println("Date(YYYY-MM-DD: ");
+                String dateledger = scanner.nextLine();
+
+                System.out.println("Name of payment on ledger  ");
+                String nameledger = scanner.nextLine();
+
+                System.out.println("How much is the payment on ledger  :  ");
+                Double amountledger = Double.valueOf(scanner.nextLine());
+
+                System.out.println("Please enter debit ledger");
+                String debitInfoledger = scanner.nextLine();
+
+            try {
+                FileWriter writer = new FileWriter ("src/ main/ resources/ transactions.csv");
+                        writer.write("Date, Name, Amount , DebitInfo \n");
+
                 // writer our trasnaction
+                for (transaction transaction : transaction) {
+                    writer.write(String.format("%s,%s,%.2f,%s\n"));
+                    transaction.getDate(),
+                            transaction.getName(),
+                            transaction.getAmount(),
+                            transaction.geDebitInfo();
 
-            for (Transaction transaction : transactions) {
-                writer.write(String.format("%s,%s,%.2f,%s\n",
+                    writer.close();
+                    System.out.println("transaction successfully.");
 
-                        transaction.getDate(),
-                        transaction.getName(),
-                        transaction.getAmount(),
-                        transaction.geDebitInfo()));
-                writer.close();
-                System.out.println("Transaction successfully.");
+                    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");//2024-04-25 10:25:08
+                    String timestamp = LocalDateTime.now().format(formatter);
 
-                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");//2024-04-25 10:25:08
-                String timestamp = LocalDateTime.now().format(formatter);
+                }
+            } catch (IOException e) {
+                throw new RuntimeException(e);}
+                break;
 
 
 
-            }
-        }catch(IOException e){
-        throw new RuntimeException(e);
+                //    List<Transaction> transactions = new ArrayList<>();
 
-        }
+        // Add sample transactions
+       // transactions.add(new Transaction("2024-05-01", "Rent", 2600.00, "Deposit"));
+       // transactions.add(new Transaction("2024-05-12", "Apple Music ", -20.00, "Withdrawal"));
+       // transactions.add(new Transaction("2024-05-29", "Chic fil A", -50.00, "Withdrawal"));
+
     }
-}
+
+
+}}}
